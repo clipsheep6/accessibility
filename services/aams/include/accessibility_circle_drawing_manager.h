e1578a65ba742c07436ab5cef759aad23083ee40
taojuncun <taojuncun@huawei.com>
2024-04-12 20:50:55 +0800

    图形库改为动态加载
    Signed-off-by: taojuncun <taojuncun@huawei.com>

--- services/aams/include/accessibility_circle_drawing_manager.h
+++ services/aams/include/accessibility_circle_drawing_manager.h
@@ -17,14 +17,10 @@
 #define ACCESSIBILITY_CIRCLE_DRAWING_MANAGER_H
 
 #include <string>
-#include "draw/canvas.h"
-#include <ui/rs_canvas_node.h>
-#include <ui/rs_surface_node.h>
-#include <ui/rs_ui_director.h>
-#include <ui/rs_root_node.h>
-#include <transaction/rs_transaction.h>
+#include <nocopyable.h>
 
 #include "accessibility_def.h"
+#include "accessibility_graphic_proxy.h"
 
 namespace OHOS {
 namespace Accessibility {
@@ -45,19 +41,16 @@ public:
     void SetPointerLocation(int32_t physicalX, int32_t physicalY, uint64_t screenId);
     void DrawingProgress(int32_t physicalX, int32_t physicalY, int32_t angle);
 
-    int32_t ParseStringToInteger(const std::string& color);
-    std::vector<int32_t> ParseColorString(const std::string& color);
-
 #ifndef USE_ROSEN_DRAWING
     void DrawingProgressByOpenSource(int32_t physicalX, int32_t physicalY, int32_t angle);
 #else
     void DrawingProgressByRosenDrawing(int32_t physicalX, int32_t physicalY, int32_t angle);
 #endif
+    std::shared_ptr<A11yRSUIDirectorProxy> rsUiDirector_ = nullptr;
+    std::shared_ptr<A11yRSSurfaceNodeProxy> surfaceNode_ = nullptr;
+    std::shared_ptr<A11yRSCanvasNodeProxy> canvasNode_ = nullptr;
+    std::shared_ptr<A11yRSNodeProxy> rootNode_ = nullptr;
 
-    std::shared_ptr<Rosen::RSUIDirector> rsUiDirector_ = nullptr;
-    std::shared_ptr<Rosen::RSSurfaceNode> surfaceNode_ = nullptr;
-    std::shared_ptr<Rosen::RSCanvasNode> canvasNode_ = nullptr;
-    std::shared_ptr<Rosen::RSNode> rootNode_ = nullptr;
     uint64_t screenId_;
     int32_t imageWidth_;
     int32_t imageHeight_;
