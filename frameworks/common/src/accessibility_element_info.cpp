af197d2cc1704b99ff83ccd43e766a2e0ee1a6a5
cwx1336461  <chenjianghua7@h-partners.com>
2024-07-25 15:22:02 +0800

    Span0725
    Signed-off-by: cwx1336461  <chenjianghua7@h-partners.com >

--- frameworks/common/src/accessibility_element_info.cpp
+++ frameworks/common/src/accessibility_element_info.cpp
@@ -984,5 +984,84 @@ int64_t AccessibilityElementInfo::GetNavDestinationId() const
 {
     return navDestinationId_;
 }
+
+void AccessibilityElementInfo::AddSpan(const SpanInfo span)
+{
+    spanList_.push_back(span);
+    HILOG_DEBUG("AddSpanListsize : [%{public}zu]", spanList_.size());
+}
+
+void AccessibilityElementInfo::SetSpanList(const std::vector<SpanInfo> &spanList)
+{
+    spanList_.clear();
+    spanList_.resize(spanList.size());
+    std::copy(spanList.begin(), spanList.end(), spanList_.begin());
+}
+
+const std::vector<SpanInfo> &AccessibilityElementInfo::GetSpanList() const
+{
+    HILOG_DEBUG("GetSpanList spanList_[0].spanid: [%{public}d]", spanList_[0].GetSpanId());
+    return spanList_;
+}
+
+SpanInfo::SpanInfo(const int32_t spanId, const std::string spanText, const std::string accessibilityText,
+    const std::string accessibilityDescription, const std::string accessibilityLevel)
+{
+    spanId_ = spanId;
+    spanText_ = spanText;
+    accessibilityText_ = accessibilityText;
+    accessibilityDescription_ = accessibilityDescription;
+    accessibilityLevel_ = accessibilityLevel;
+}
+
+void SpanInfo::SetSpanId(const int32_t spanId)
+{
+    spanId_ = spanId;
+}
+
+void SpanInfo::SetSpanText(const std::string spanText)
+{
+    spanText_ = spanText;
+}
+
+void SpanInfo::SetAccessibilityText(const std::string accessibilityText)
+{
+    accessibilityText_ = accessibilityText;
+}
+
+void SpanInfo::SetAccessibilityDescription(const std::string accessibilityDescription)
+{
+    accessibilityDescription_ = accessibilityDescription;
+}
+
+void SpanInfo::SetAccessibilityLevel(const std::string accessibilityLevel)
+{
+    accessibilityLevel_ = accessibilityLevel;
+}
+
+int32_t SpanInfo::GetSpanId() const
+{
+    return spanId_;
+}
+
+const std::string &SpanInfo::GetSpanText() const
+{
+    return spanText_;
+}
+
+const std::string &SpanInfo::GetAccessibilityText() const
+{
+    return accessibilityText_;
+}
+
+const std::string &SpanInfo::GetAccessibilityDescription() const
+{
+    return accessibilityDescription_;
+}
+
+const std::string &SpanInfo::GetAccessibilityLevel() const
+{
+    return accessibilityLevel_;
+}
 } // namespace Accessibility
 } // namespace OHOS
